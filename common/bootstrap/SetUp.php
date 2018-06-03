3e88b0bd3578b9997aea68fc1d7e5dd78029b77a
Aleksey-Mmm <alex@proplus.ru>
2018-06-03 22:23:42 +0900

    urok1 04:40

---
+++ common/bootstrap/SetUp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 03.06.2018
+ * Time: 18:04
+ */
+
+namespace common\bootstrap;
+
+
+use yii\base\BootstrapInterface;
+
+class SetUp implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+}
